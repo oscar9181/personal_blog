4f3246f9751cd25562bab22c0807245a3d00eaf8
oscar9181 <oscarokola17@gmail.com>
2022-05-15 15:15:42 +0300

    login.html

--- personal_blog/post.py
+++ personal_blog/post.py
@@ -20,5 +20,17 @@ def home():
     """
     return render_template('home.html',posts=posts)
 
+@app.route("/register")
+def register():
+    form=RegistrationForm
+    return render_template('register.html',title='Register',form=form)
+
+
+@app.route("/login")
+def login():
+    form=LoginForm
+    return render_template('login.html',title='Login',form=form)
+
+
 if __name__== '__main__':
      app.run(debug = True)
