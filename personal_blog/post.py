77fcee45af43ec793cbb4afae516c02bc16ef0c4
oscar9181 <oscarokola17@gmail.com>
2022-05-15 13:26:22 +0300

    navbar

---
+++ personal_blog/post.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+posts = [
+   {
+    'author' :'Sophie Paxton',
+    'title'  :'Interface Design',
+    'pitch'  :'Animation is like cursing. If you overuse it, it loses all its impact.',
+    'date_posted' : 'October 17 2002'
+   }]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    """
+    View root page function that returns the home page and its data
+    """
+    return render_template('home.html',posts=posts)
+
+if __name__== '__main__':
+     app.run(debug = True)
