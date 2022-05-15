0209480f681aa6c836bbdf1f02680dbd0859b533
oscar9181 <oscarokola17@gmail.com>
2022-05-15 15:02:20 +0300

    register form

--- personal_blog/forms.py
+++ personal_blog/forms.py
@@ -1,1 +1,15 @@
+from operator import eq
 from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired,Length,Email,EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username= StringField('Username',
+                          validators=[DataRequired(),Length(min=4,max=20)])
+    email= StringField('email',
+                       validators=[DataRequired(),Email()])
+    password= PasswordField('Password',validators=[DataRequired()])
+    confirm_password= PasswordField('Confirm Password',
+                       validators=[DataRequired(),EqualTo('password')])
+    submit= SubmitField('Signup')
