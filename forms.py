55a7736ab992fee19521906ba059889e534e7adf
oscar9181 <oscarokola17@gmail.com>
2022-05-16 15:40:06 +0300

    database

--- forms.py
+++ forms.py
@@ -1,4 +1,3 @@
-from operator import eq
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,Email,EqualTo
---
+++ post.py
@@ -0,0 +1,81 @@
+from datetime import datetime
+from flask import Flask,render_template,url_for, flash,redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm,LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='40d3625a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(20),unique=True,nullable=False)
+    email = db.Column(db.String(100),unique=True,nullable=False)
+    image_file = db.Column(db.String(20),nullable=False,default='default.jpg')
+    password = db.Column(db.String(20),nullable=False,)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"Post('{self.content}', '{self.date_posted}')"
+    
+posts = [
+   {
+    'author' :'Sophie Paxton',
+    'title'  :'Interface Design',
+    'content'  :'Animation is like cursing. If you overuse it, it loses all its impact.',
+    'date_posted' : 'October 17 2002'
+   }]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    """
+    View root page function that returns the home page and its data
+    """
+    return render_template('home.html',posts=posts)
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form. validate_on_submit():
+        flash('Your account hass been created!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
+
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form. validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+
+if __name__== '__main__':
+     app.run(debug = True)
